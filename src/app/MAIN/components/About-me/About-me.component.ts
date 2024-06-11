0386a97ff21c710786e56edc31f4b66f6b6ff310
sergio <sergiocastro86@gmail.com>
2024-06-11 18:14:44 -0500

    RESUME CHANGES

--- src/app/MAIN/components/About-me/About-me.component.ts
+++ src/app/MAIN/components/About-me/About-me.component.ts
@@ -6,4 +6,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './About-me.component.html',
   styleUrl: './About-me.component.css'
 })
-export class AboutMeComponent { }
+
+
+
+export class AboutMeComponent {
+
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/RESUME KEVIN CASTRO';
+    link.download = 'RESUME KEVIN CASTRO.pdf';
+    link.click();
+  }
+
+}
+
+
